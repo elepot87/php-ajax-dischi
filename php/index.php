ea6723de1649324d47a277560fe6266a55f1d109
elepot87 <eleonorapotente@gmail.com>
2021-12-21 14:46:50 +0100

    set up markup per parte in solo php

---
+++ php/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php dischi</title>
+    <!-- Main style -->
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <header class='header'>
+        <div class="container">
+            <img src="./img/logo.png" alt="logo spotify" class="logo">
+        </div>
+    </header>
+    <!-- Main -->
+    <main>
+        <div class="container">
+            <ul class="main-list">
+                <li class="list-item">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="" class="cover">
+                    <div class="info-album">
+                        <h3 class="title-cd">prova</h3>
+                        <div class="author">Bon Jovi</div>
+                        <div class="year">1988</div>
+                        <div class="genre">Rock</div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </main>
+</body>
+
+</html>
